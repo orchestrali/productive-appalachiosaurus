91b7c354f4d2b24f76d8f02929b21af5e5b2af3a
orchestrali <orchestrali@gmail.com>
2025-09-13 17:44:00 +0100

    Update getmethods.js

--- src/getmethods.js
+++ src/getmethods.js
@@ -2,13 +2,14 @@ const connect = require('./mongoose/connect.js');
 const find = require('./find/findFields.js');
 
 var query = {
-  query: {},
+  query: {stage: 4},
   fields: "title stage class ccNum pnFull oldtitle"
 };
 
 module.exports = function getmethods(cb) {
   var db = connect();
   find("method", query, (results) => {
+    console.log(results.length);
     //I could just pass the callback to find, but in case I want to do anything...
     cb(results);
   });
