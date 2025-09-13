ec9d15f47f4d0e467e804130b822bdae3096fb3e
orchestrali <orchestrali@gmail.com>
2025-09-13 16:24:21 +0100

    explain how it works

--- src/dovebells.js
+++ src/dovebells.js
@@ -2,18 +2,30 @@ const fs = require('fs');
 const stream = require('stream');
 var towers = require('./towers.json');
 
+//add bell info to towers
 module.exports = function doveformat() {
   let csv = fs.readFileSync("dove/bells.csv", "utf8");
+  //keys or column headers that I want
   let needed = ["BellID", "TowerID", "RingID", "BellRole", "Weight(lbs)", "Weight(approx)", "CastDate"];
+  //indexes of the columns I want
   let needi = [];
+  //building column headers from the document, getting their order
   let keys = [];
+  //holder of arrays of values for each bell
   let bells = [];
+  //current string within the csv
   let current = "";
+  //first row of csv or not
   let first = true;
+  //holder for current row of csv
   let arr = [];
+  //current column index
   let i = 0;
+  //signal to stop reading csv
   let end = false;
+  //character of csv
   let key = 1;
+  //inside quotation marks - commas within column/value
   let quote = false;
   
   loop();
@@ -35,8 +47,10 @@ module.exports = function doveformat() {
             i++;
           } else {
             if (quote) {
+              //within quotes, comma is part of value
               current += ",";
             } else if (needi.includes(i)) {
+              //end of column, add current value to arr
               arr.push(current);
               current = "";
               i++;
@@ -48,12 +62,14 @@ module.exports = function doveformat() {
           break;
         case "\n":
           if (first) {
+            //end of header row
             if (needed.includes(current)) {
               keys.push(current);
               needi.push(i);
             }
             first = false;
           } else {
+            //end of row, add bell values to array
             if (needi.includes(i)) arr.push(current);
             bells.push(arr);
             //if (bells.length === 1) console.log(arr.length);
@@ -80,6 +96,8 @@ module.exports = function doveformat() {
       keys.forEach((k,j) => {
         o[k] = nums.includes(k) ? Number(arr[j]) : arr[j];
       });
+      //this indicates bell is part of a chime
+      //some have form "c3" just chime, some "3c3" full-circle ring AND chime
       let c = o["BellRole"].indexOf("c");
       if (c) { //i.e. c != 0
         if (c > 0) o["BellRole"] = o["BellRole"].slice(0,c);
