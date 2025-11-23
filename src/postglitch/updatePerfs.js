5c2cf301de73c8669587465ebd3e3d07d109597b
orchestrali <orchestrali@gmail.com>
2025-11-23 13:38:23 +0000

    Update updatePerfs.js

--- src/postglitch/updatePerfs.js
+++ src/postglitch/updatePerfs.js
@@ -1,6 +1,21 @@
-
+const findFields = require("../find/findFields.js");
+const find = require("../find/find.js");
 //I want to fix ampersands
 //and add methodTitles to performances
 module.exports = function updateperfs() {
+  //connect here?
+
+  let q = {query: {performances: {$not: {$size: 0}}}, fields: "title"};
+  findFields("method", q, (res) => {
+    let idindex = {};
+    res.forEach(o => {
+      idindex[o._id] = o.title;
+    });
+
+    //next get some performances
+    let pq = {methodTitle: {$exists: false}}; //probably want more filter than that
+    find("performance", pq, (perfs) => {});
+    
+  });
   
 }
