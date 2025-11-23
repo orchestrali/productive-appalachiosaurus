7f5f0f05e6ff2e4f4ed6525b25b9f590a3fe7418
orchestrali <orchestrali@gmail.com>
2025-11-23 14:55:08 +0000

    add notes and stuff

--- src/postglitch/updatePerfs.js
+++ src/postglitch/updatePerfs.js
@@ -1,5 +1,9 @@
 const findFields = require("../find/findFields.js");
 const find = require("../find/find.js");
+
+//okay really I should just be rebuilding the method database and performances with it, to catch the firsts I wasn't looking for
+//probably some of the current (Nov 2025) performances are from all the duplicate methods
+
 //I want to fix ampersands
 //and add methodTitles to performances
 module.exports = function updateperfs() {
@@ -16,18 +20,24 @@ module.exports = function updateperfs() {
     let pq = {methodTitle: {$exists: false}}; //probably want more filter than that
     find("performance", pq, (perfs) => {
       console.log(perfs.length + " performances");
-      let stop = Math.min(200, perfs.length);
-      let i = 0;
+      
       let problemcount = 0;
+      let filtered = [];
       perfs.forEach(p => {
         let t = idindex[p.method];
-        t ? p.methodTitle = t : problemcount++;
+        if (t) {
+          p.methodTitle = t;
+          filtered.push(p);
+        }
       });
 
-      console.log("problemcount: "+problemcount);
+      console.log("problemcount: "+(perfs.length-filtered.length));
+      let stop = Math.min(200, filtered.length);
+      let i = 0;
 
       function updateloop() {
-        //wait on this... 
+        let p = filtered[i];
+        p.save().then(); // .........
       }
     });
     
