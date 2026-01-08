b4e85cbc4b126f4c6458f12d547d0f5070dd01a7
orchestrali <orchestrali@gmail.com>
2026-01-08 23:55:47 +0000

    clickable to get methods???

--- public/scripthunt.js
+++ public/scripthunt.js
@@ -1,8 +1,28 @@
 const places = "1234567890ETABCD";
 
+var huntpaths;
 
 
+$(function() {
+  huntpaths = window.huntpaths;
+  $(".clickable").on("click", clicktablecell);
+});
 
-$(function() {});
 
-
+function clicktablecell(e) {
+  if (!$(this).hasClass("selected")) {
+    $("#methodcontainer li").remove();
+    $("td.selected").removeClass("selected");
+    $(this).addClass("selected");
+    let id = this.id;
+    let arr = id.split("-");
+    let stage = Number(arr[arr.length-1]);
+    arr.pop();
+    let key = arr.join("-");
+    let mm = huntpaths.single.plain.methods.filter(m => m.stage === stage);
+    mm.forEach(m => {
+      let str = `<li><a href="https://complib.org/method/${m.ccnum}" target="blank">${m.title}</a></li>`;
+      $("#methodcontainer ul").append(str);
+    });
+  }
+}
