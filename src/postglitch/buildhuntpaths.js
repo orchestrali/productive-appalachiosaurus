6d92214450afd4960375d40cb62efe38c552ce93
orchestrali <orchestrali@gmail.com>
2026-01-08 15:47:16 +0000

    add scripts

--- src/postglitch/buildhuntpaths.js
+++ src/postglitch/buildhuntpaths.js
@@ -1,7 +1,11 @@
 const findFields = require("../find/findFields.js");
-const pagestart = `<head>
+const pagestart = [`<head>
   <title>Hunt bell paths</title>
   <link rel="stylesheet" href="/stylehunt.css">
+  <script>
+  `,
+                   `
+  </script>
 </head>
 <body>
 <h2>Single hunt methods</h2>
@@ -22,11 +26,13 @@ const pagestart = `<head>
       </tr>
     </thead>
     <tbody>
-`;
+`];
 const pageend = `
     </tbody>
   </table>
 </div>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="scripthunt.js"></script>
 </body>
 `;
 
@@ -51,7 +57,8 @@ module.exports = function buildhuntpaths(cb) {
   findFields("method", query, (res) => {
     res.forEach(m => analyzesingleplain(m.huntPath, m.title, m.stage));
     let tbody = buildsingleplaintable();
-    let page = pagestart + tbody + pageend;
+    let str = `window.huntpaths = `+ JSON.stringify(huntpaths);
+    let page = pagestart[0] + str + pagestart[1] + tbody + pageend;
     cb(page);
   });
   
