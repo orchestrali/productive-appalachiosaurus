a7a688d8c6dbf9c367cb9c329fac59bd4f2de5ec
orchestrali <orchestrali@gmail.com>
2026-01-10 01:10:45 +0000

    prepare for multiple tables

--- public/scripthunt.js
+++ public/scripthunt.js
@@ -22,8 +22,9 @@ function clicktablecell(e) {
     let arr = id.split("-");
     let stage = Number(arr[arr.length-1]);
     arr.pop();
-    let key = arr.join("-");
-    let mm = huntpaths.single.plain[key].methods.filter(m => m.stage === stage);
+    let keys = [arr[0],arr[1],arr.slice(2).join("-")];
+    
+    let mm = huntpaths[keys[0]][keys[1]][keys[2]].methods.filter(m => m.stage === stage);
     mm.forEach(m => {
       let str = `<li><a href="https://complib.org/method/${m.ccnum}" target="blank">${m.title}</a></li>`;
       $("#methodcontainer ul").append(str);
